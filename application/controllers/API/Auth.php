1154376332a38c250d4aaad3e63983adc5d259fe
Zulfa Khoerul <zulfakhoerul00@gmail.com>
2021-08-24 23:54:15 +0700

    api login berhasil

--- application/controllers/API/Auth.php
+++ application/controllers/API/Auth.php
@@ -11,22 +11,39 @@ class Auth extends RestController
 		parent::__construct($config);
 		$this->load->database();
 		$this->load->model('AuthModel');
-        $this->load->library('form_validation');
+        // $this->load->library('form_validation');
 	}
 
 	public function login_post()
     {
-        $post = array(
-            'email'     => $this->input->post('email'),
-            'password'  => $this->input->post('password')
-        );
-
+        $email     = $this->input->post('email');
+        $password  = $this->input->post('password');
+            // $where = array(
+            //     'email' => $email,
+            //     'password' => password_hash($password, PASSWORD_BCRYPT)
+            // );
+        
+        $result = $this->AuthModel->getUser($email, $password);
+			if($result == true){
+                foreach($result as $row){
+                    $sess_data = array(
+                        'email'      => $row->email,
+                        'nama'       => $row->nama,
+                        'role'       => $row->role
+                    );
+                }
+                    $this->response(array(
+                        'status'  => true,
+                        'message' => 'login success',
+                        'data'    => $sess_data
+                    ),200);
+            }
     }
 
     public function logout()
     {
         // hancurkan semua sesi
         $this->session->sess_destroy();
-        redirect(site_url('login'));
+        // redirect(site_url('login'));
     }
 }
