09fdfe36ca699ee4c2dcb6dedc611a82d21dde07
dimasaddriansyah <dimasaddriansyah14@gmail.com>
2021-08-25 15:22:00 +0700

    Log 25 Agustus 2021 | Consume API Login

--- application/views/layouts/superAdmin/header.php
+++ application/views/layouts/superAdmin/header.php
@@ -8,7 +8,7 @@
 	<ul class="navbar-nav navbar-right">
 		<li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
 				<img alt="image" src="<?= base_url('assets/img/avatar/avatar-1.png') ?>" class="rounded-circle mr-1">
-				<div class="d-sm-none d-lg-inline-block">Super Admin</div>
+				<div class="d-sm-none d-lg-inline-block"> <?= $this->session->userdata("nama") ?> </div>
 			</a>
 			<div class="dropdown-menu dropdown-menu-right">
 				<div class="dropdown-title">Menu List</div>
