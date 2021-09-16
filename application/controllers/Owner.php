7aec53daf89254e64396fb94ed27f4fab50e7729
Verliana NS <verliananss23000@gmail.com>
2021-09-17 02:23:47 +0700

    revisi laporan tabel pdf

--- application/controllers/Owner.php
+++ application/controllers/Owner.php
@@ -743,19 +743,19 @@ class Owner extends CI_Controller
 
 	}
 
-	public function foto_produk_tambah()
-	{
-		// arahkan ke url atau lokasi gambar berada
-		$getAPI = $this->curl->simple_get($this->api . 'FotoProduk');
+	// public function foto_produk_tambah()
+	// {
+	// 	// arahkan ke url atau lokasi gambar berada
+	// 	$getAPI = $this->curl->simple_get($this->api . 'FotoProduk');
 
 
-		$datas = json_decode($getAPI, true);
+	// 	$datas = json_decode($getAPI, true);
 
-		$data = array('foto_produks' => $datas["data"]);
+	// 	$data = array('foto_produks' => $datas["data"]);
 
-		// $data ini masukan ke json
-		$this->template->load('layouts/owner/master', 'dashboard/owner/foto_produk/tambah', $data);
-	}
+	// 	// $data ini masukan ke json
+	// 	$this->template->load('layouts/owner/master', 'dashboard/owner/foto_produk/tambah', $data);
+	// }
 
 	public function do_upload()
 	{
--- application/views/dashboard/owner/laporan/transaksi/index_pdf.php
+++ application/views/dashboard/owner/laporan/transaksi/index_pdf.php
@@ -4,40 +4,64 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Laporan Transaksi</title>
+		<style>
+            #table {
+                font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+                border-collapse: collapse;
+                width: 100%;
+            }
+
+            #table td, #table th {
+                border: 1px solid #ddd;
+                padding: 8px;
+            }
+
+            #table tr:nth-child(even){background-color: #f2f2f2;}
+
+            #table tr:hover {background-color: #ddd;}
+
+            #table th {
+                padding-top: 10px;
+                padding-bottom: 10px;
+                text-align: left;
+                background-color: #d3d3d3;
+                color: black;
+            }
+        </style>
     </head>
     <body>
         <div style="text-align:center">
             <h3> Laporan Transaksi </h3>
-        </div> 
+        </div> <br>
         <div class="section-body">
 			<div class="card">
 				<div class="card-body">
 					<div class="table-responsive">
-						<table cellpadding="10" width="100%" >
-								<thead class="thead-dark" align="center">
+						<table id="table">
+								<thead class="thead-dark">
 									<tr>
-                                        <th class="text-center">No.</th>
-                                        <th class="text-center">Tanggal Transaksi</th>
-                                        <th class="text-center">Nama Customer</th>
-                                        <th class="text-center">Jenis Transaksi</th>
-                                        <th class="text-center">Status</th>
-                                        <th class="text-center">Diskon</th>
-                                        <th class="text-center">Pembayaran</th>
-                                        <th class="text-center">Total Transaksi</th>
+                                        <th>No.</th>
+                                        <th>Tanggal Transaksi</th>
+                                        <th>Nama Customer</th>
+                                        <th>Jenis Transaksi</th>
+                                        <th>Status</th>
+                                        <th>Diskon</th>
+                                        <th>Pembayaran</th>
+                                        <th>Total Transaksi</th>
 									</tr>
 								</thead>
 								<?php if (!empty($transaksi)) { ?>
 									<?php foreach ($transaksi as $no => $row) : ?>
-										<tbody align="center">
+										<tbody>
 											<tr>
-												<td class="text-center"><?= ++$no ?></td>
-												<td class="text-center"><?= $row["tggl_transaksi"] ?></td>
-												<td class="text-center"><?= $row["nama_cust"] ?></td>
-												<td class="text-center"><?= $row["jenis_transaksi"] ?></td>
-												<td class="text-center"><?= $row["status"] ?></td>
-                                                <td class="text-center"><?= $row["diskon"] ?></td>
-												<td class="text-center"><?= $row["bayar"] ?></td>
-												<td class="text-center"><?= $row["total_transaksi"] ?></td>
+												<td><?= ++$no ?></td>
+												<td><?= $row["tggl_transaksi"] ?></td>
+												<td><?= $row["nama_cust"] ?></td>
+												<td><?= $row["jenis_transaksi"] ?></td>
+												<td><?= $row["status"] ?></td>
+                                                <td><?= $row["diskon"] ?></td>
+												<td><?= $row["bayar"] ?></td>
+												<td><?= $row["total_transaksi"] ?></td>
 											</tr>
 										</tbody>
 									<?php endforeach; ?>
